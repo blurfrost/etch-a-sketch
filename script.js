b61ce961ee3249ca1c1ec285f6e0fcc4252c0448
blurfrost <ksyc07@gmail.com>
2024-05-05 16:14:04 +0800

    Created HTML, CSS and Javascript files and added initial 16x16 square grid

---
+++ script.js
@@ -0,0 +1,15 @@
+// relate to gridcontainer div in html
+const gridContainer = document.querySelector("#gridcontainer");
+
+// iterate 16 times to create 16 rows of containers
+for (i = 0; i < 16; i++) {
+    const container = document.createElement("div");
+    container.classList.add("container");
+    gridContainer.appendChild(container);
+    // iterate 16 times to create 16 children squares in each container
+    for (j = 0; j < 16; j++) {
+        const square = document.createElement("div");
+        square.classList.add("square");
+        container.appendChild(square);
+    }
+} 
