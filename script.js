21a876dc1b20adca35f8153c1be8313486b98458
blurfrost <ksyc07@gmail.com>
2024-05-05 18:05:15 +0800

    Updated code to ensure that the size of the grid stays fixed regardless of the number of squares on a side

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
 // relate to gridcontainer div in html
 const gridContainer = document.querySelector("#gridcontainer");
+const invalidInput = document.querySelector("#invalid");
+const confirmButton = document.querySelector("#confirm");
+const squareInput = document.querySelector("#quantity");
 
-// iterate 16 times to create 16 rows of containers
-for (i = 0; i < 16; i++) {
-    const container = document.createElement("div");
-    container.classList.add("container");
-    gridContainer.appendChild(container);
-    // iterate 16 times to create 16 children squares in each container
-    for (j = 0; j < 16; j++) {
-        const square = document.createElement("div");
-        square.classList.add("square");
-        square.addEventListener('mouseover', ()=> {
-            square.classList.add("hover");
-        })
-        container.appendChild(square);
-        
-    }
-} 
+let gridQuantitySquares = 16;
+createGrid(gridQuantitySquares);
+
+function createGrid(squares) {
+    // iterate 16 times to create 16 rows of containers
+    for (i = 0; i < squares; i++) {
+        const container = document.createElement("div");
+        container.classList.add("container");
+        gridContainer.appendChild(container);
+        // iterate 16 times to create 16 children squares in each container
+        for (j = 0; j < squares; j++) {
+            const square = document.createElement("div");
+            square.classList.add("square");
+            square.addEventListener("mouseover", () => {
+                square.classList.add("hover");
+            });
+            container.appendChild(square);
+        }
+    } 
+}
