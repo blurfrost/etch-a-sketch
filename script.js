bd3861a383ef9496ad2cc5f66ff157b68b408021
blurfrost <ksyc07@gmail.com>
2024-05-05 16:40:40 +0800

    Added event listener to change colour of the current square div on mouse hover

--- script.js
+++ script.js
@@ -10,6 +10,10 @@ for (i = 0; i < 16; i++) {
     for (j = 0; j < 16; j++) {
         const square = document.createElement("div");
         square.classList.add("square");
+        square.addEventListener('mouseover', ()=> {
+            square.classList.add("hover");
+        })
         container.appendChild(square);
+        
     }
 } 
