2058f7dd796134e9a5591b16069300c3767064f3
blurfrost <ksyc07@gmail.com>
2024-05-05 23:04:08 +0800

    Added function to change the grid size to between 1 and 100 squares on each side through an input element

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 // relate to gridcontainer div in html
 const gridContainer = document.querySelector("#gridcontainer");
-const invalidInput = document.querySelector("#invalid");
+const gridSize = document.querySelector("#gridsize");
 const confirmButton = document.querySelector("#confirm");
 const squareInput = document.querySelector("#quantity");
 
-let gridQuantitySquares = 16;
+// set initial number of squares on each side
+const gridQuantitySquares = 16;
+displayGridSize(gridQuantitySquares);
 createGrid(gridQuantitySquares);
 
+// function to change text dynamically based on no. of squares on each side
+function displayGridSize(squares) {
+    if (!squares) {
+        gridSize.textContent = "Invalid input! Please input a number from 1 to 100."
+    }
+    else {
+        gridSize.textContent = "Current size of grid: " + squares + " x " + squares;
+    }
+}
+
+// function to create grid when page initially loads, and when a valid input is entered in the text box
 function createGrid(squares) {
     // iterate 16 times to create 16 rows of containers
     for (i = 0; i < squares; i++) {
@@ -23,4 +36,25 @@ function createGrid(squares) {
             container.appendChild(square);
         }
     } 
+}
+
+// event listener to change number of squares on each side of the grid, when confirm button is pressed
+confirmButton.addEventListener("click", () => {
+    newSquares = squareInput.value;
+    // clear value of text box
+    squareInput.value = "";
+    console.log(newSquares);
+    if (newSquares > 0 && newSquares <= 100) {
+        removeGrid(newSquares);
+    }
+    else {
+        newSquares = 0;
+    }
+    displayGridSize(newSquares);
+});
+
+// function to completely delete the old grid, then regenerate a new grid based on the input in the text box
+function removeGrid(squares) {
+    gridContainer.innerHTML = "";
+    createGrid(squares);
 }
